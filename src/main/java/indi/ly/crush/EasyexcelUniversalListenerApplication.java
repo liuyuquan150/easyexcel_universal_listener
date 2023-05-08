7ad63b1a485337e78b6c7a7211b03df73c4fb438
liuyuquan150 <liu_yu_quan@foxmail.com>
2023-05-08 11:46:11 +0800

    :bulb: 添加、修改注释

--- src/main/java/indi/ly/crush/EasyexcelUniversalListenerApplication.java
+++ src/main/java/indi/ly/crush/EasyexcelUniversalListenerApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * <h2>主启动应用程序</h2>
+ * <p>
+ *     现有开源组件: <a href="https://github.com/pig-mesh/excel-spring-boot-starter">excel-spring-boot-starter</a>
+ * </p>
  *
  * @author 云上的云
  * @since 1.0
